516478fcf87cd20009f2eba8e9b7bf14c96f55f3
Mate Soos <soos.mate@gmail.com>
2024-02-27 21:54:37 +0100

    Adding copyright

--- src/sbva.cpp
+++ src/sbva.cpp
@@ -1,5 +1,6 @@
 /******************************************
 Copyright (C) 2023 Andrew Haberlandt, Harrison Green, Marijn J.H. Heule
+              2024 Changes, maybe bugs by Mate Soos
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
